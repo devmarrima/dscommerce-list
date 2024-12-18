17f9b2f9ae7c49162e25569f8724332fcf0edd0a
curso_básico_Dio <marrimacelso@gmail.com>
2024-12-17 23:00:46 -0300

    primeiro commit

---
+++ src/main/java/com/devmarrima/dscommerce_list/DscommerceListApplication.java
@@ -0,0 +1,13 @@
+package com.devmarrima.dscommerce_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmarrima/dscommerce_list/DscommerceListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarrima.dscommerce_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
