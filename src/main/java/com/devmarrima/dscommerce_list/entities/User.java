0297f1dd7100ae6acbb7eee712dcc65452f14f89
curso_básico_Dio <marrimacelso@gmail.com>
2024-12-17 23:37:58 -0300

    Entidade User, banco H2

---
+++ src/main/java/com/devmarrima/dscommerce_list/entities/User.java
@@ -0,0 +1,89 @@
+package com.devmarrima.dscommerce_list.entities;
+
+	import java.time.LocalDate;
+
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.GeneratedValue;
+	import jakarta.persistence.GenerationType;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.Table;
+
+	@Entity
+	@Table(name = "tb_user")
+	public class User {
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private long id;
+	    private String name;
+	    private String email;
+	    private String phone;
+	    private LocalDate birthDate;
+	    private String passWord;
+
+	    public User() {
+
+	    }
+
+	    public User(long id, String name, String email, String phone, LocalDate birthDate, String passWord) {
+	        this.id = id;
+	        this.name = name;
+	        this.email = email;
+	        this.phone = phone;
+	        this.birthDate = birthDate;
+	        this.passWord = passWord;
+	    }
+
+	    public long getId() {
+	        return id;
+	    }
+
+	    public void setId(long id) {
+	        this.id = id;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public String getPhone() {
+	        return phone;
+	    }
+
+	    public void setPhone(String phone) {
+	        this.phone = phone;
+	    }
+
+	    public LocalDate getBirthDate() {
+	        return birthDate;
+	    }
+
+	    public void setBirthDate(LocalDate birthDate) {
+	        this.birthDate = birthDate;
+	    }
+
+	    public String getPassWord() {
+	        return passWord;
+	    }
+
+	    public void setPassWord(String passWord) {
+	        this.passWord = passWord;
+	    }
+
+	    
+
+
+
+	}
+
