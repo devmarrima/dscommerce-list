c382b9678f209ceb32a956bfe951919dff9feb3f
curso_básico_Dio <marrimacelso@gmail.com>
2024-12-18 22:22:23 -0300

    Payment, relacionamento um-para-um

--- src/main/java/com/devmarrima/dscommerce_list/entities/Order.java
+++ src/main/java/com/devmarrima/dscommerce_list/entities/Order.java
@@ -2,6 +2,7 @@ package com.devmarrima.dscommerce_list.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +27,61 @@ public class Order {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment payment;
+
+	public Order() {
+	}
+
+	public Order(long id, Instant moment, OrderStatus orderStatus, User client, Payment payment) {
+		this.id = id;
+		this.moment = moment;
+		this.orderStatus = orderStatus;
+		this.client = client;
+		this.payment = payment;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
 	
 	
 
