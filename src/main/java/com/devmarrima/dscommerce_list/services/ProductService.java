e8858ee142e719f9ce9673ba4fd6c9332c05345e
curso_básico_Dio <marrimacelso@gmail.com>
2025-01-10 21:27:59 -0300

    new product PUT & corrigir o método <copyDtoToEntity>

--- src/main/java/com/devmarrima/dscommerce_list/services/ProductService.java
+++ src/main/java/com/devmarrima/dscommerce_list/services/ProductService.java
@@ -5,17 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.devmarrima.dscommerce_list.dto.ProductDTO;
 import com.devmarrima.dscommerce_list.entities.Product;
 import com.devmarrima.dscommerce_list.repositories.ProductRepository;
 
-@ Service
+@Service
 public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 		Product product = repository.findById(id).get();
@@ -31,12 +30,24 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		entity.setId(dto.getId());
-		entity.setName(dto.getName());
-		entity.setDescription(dto.getDescription());
-		entity.setImgUrl(dto.getImgUrl());
-
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product entity = repository.getReferenceById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+
 	}
 }
