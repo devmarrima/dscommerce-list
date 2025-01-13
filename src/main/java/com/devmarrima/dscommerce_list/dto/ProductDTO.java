22764565dd97f20055161a2bfe22b71038dd2bde
curso_básico_Dio <marrimacelso@gmail.com>
2025-01-13 00:13:50 -0300

    Bean validation

--- src/main/java/com/devmarrima/dscommerce_list/dto/ProductDTO.java
+++ src/main/java/com/devmarrima/dscommerce_list/dto/ProductDTO.java
@@ -1,15 +1,23 @@
 package com.devmarrima.dscommerce_list.dto;
 
 import com.devmarrima.dscommerce_list.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
-	
+
 	private Long id;
+	@Size(min = 3, max = 80, message = " O campo tem que ter de 3 a 80 caracteres")
+	@NotBlank(message = "Campo requerido") 
 	private String name;
+	@Size(min = 10, message = " O campo tem que ter no mínimo 10 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String description;
+	@Positive(message = "O preço tem que ser positivo")
 	private Double price;
 	private String imgUrl;
-	
+
 	public ProductDTO() {
 
 	}
@@ -21,15 +29,14 @@ public class ProductDTO {
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
-	
+
 	public ProductDTO(Product entity) {
 		id = entity.getId();
 		name = entity.getName();
-		description =entity.getDescription();
+		description = entity.getDescription();
 		price = entity.getPrice();
 		imgUrl = entity.getImgUrl();
 	}
-	
 
 	public Long getId() {
 		return id;
@@ -51,6 +58,4 @@ public class ProductDTO {
 		return imgUrl;
 	}
 
-	
-	
 }
---
+++ src/main/java/com/devmarrima/dscommerce_list/dto/ValidationError.java
@@ -0,0 +1,19 @@
+package com.devmarrima.dscommerce_list.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError {
+    public List<FieldMessage> erros = new ArrayList<>();
+
+    public ValidationError(Instant timeStamp, Integer status, String error, String path) {
+        super(timeStamp, status, error, path);
+    }
+
+    public void addError(String fieldName, String massege){
+        erros.add(new FieldMessage(fieldName, massege));
+    }
+    
+
+}
